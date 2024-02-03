9ce504315acde65870d630281d29bf352d5cbe91
Akshay Prabhu <12824090+akshayprabhu200@users.noreply.github.com>
2024-02-03 23:32:54 +0530

    Increased responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,16 +14,16 @@ export default function Home() {
           </ul>
         </nav>
       </header>
-      <main className="grid grid-cols-12">
-        <section className="col-start-3 col-end-10">
-          <h3 className="text-4xl ">Thinking of selling your webite online?</h3>
+      <main className="grid grid-cols-12 gap-10">
+        <section className="col-start-2 col-end-10 md:col-start-3 md:col-end-6">
+          <h3 className="text-4xl">Thinking of selling your webite online?</h3>
           <h2 className="text-6xl text-headingText">
             Find the <span className="text-flippaBlue">optimum</span> price to
             sell your website
           </h2>
         </section>
 
-        <section className="col-start-3 col-end-10 border-flippaBlue rounded-3xl min-h-600 border-4 w-600 p-[5rem]">
+        <section className="col-start-2 col-end-10 md:col-start-6 md:col-end-10 border-flippaBlue rounded-3xl md:min-h-600 border-4 w-600 p-[5rem]">
           <Calculator></Calculator>
         </section>
       </main>
